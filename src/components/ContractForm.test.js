053a8756c5ee61e0af6c6c343552ed27d52ea182
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-09 15:26:14 -0700

    expected behavior tested

--- src/components/ContractForm.test.js
+++ src/components/ContractForm.test.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import ContractForm from './ContactForm';
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import ContractForm from './ContactForm'
+import { act } from 'react-dom/test-utils'
 
 test("renders ComponentForm without crashing", () => {
-  rtl.render(<ContractForm />);
-});
+  rtl.render(<ContractForm />)
+})
+
+test("fills out form, submits, checks for return (expected behavior)", () => {
+    const { getByTestId, findByTestId } = rtl.render(<ContractForm/>)
+
+    const firstName = getByTestId('firstName input');
+    const lastName = getByTestId('lastName input');
+    const email = getByTestId('email input');
+    const message = getByTestId('message input');
+
+    // Fill out form inputs
+        rtl.fireEvent.change(firstName, {
+            target: {name: 'firstName', value: 'Ron'}
+        });
+    
+        rtl.fireEvent.change(lastName, {
+            target: {name: 'lastName', value: 'Weasley'}
+        });
+    
+        rtl.fireEvent.change(email, {
+            target: {name: 'email', value: 'ronaldweasley@hogwarts.com'}
+        });
+    
+        rtl.fireEvent.change(message, {
+            target: {name: 'message', value: 'I solemly swear that I am up to no good.'}
+        });
+    
+        // Press submit input
+        rtl.fireEvent.click(getByTestId('submit input'))
+
+        // Got the return
+        findByTestId('return')
+})
