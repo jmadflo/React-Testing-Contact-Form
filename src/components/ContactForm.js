8439aa6a19197f4061bf8f286599864e604d7dcc
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-10 01:42:49 -0700

    STRETCH - added select and checkbox to form and tested them

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,7 +19,7 @@ const ContactForm = () => {
             data-testid="firstName input"
             name="firstName"
             placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, minLength: 3 })}
           />
           {errors.firstName && (
             <p data-testid="firstName error">Looks like there was an error: {errors.firstName.type}</p>
@@ -52,6 +52,35 @@ const ContactForm = () => {
           <label htmlFor="message">Message</label>
           <textarea data-testid="message input" name="message" ref={register({ required: false })} />
         </div>
+
+        <div>
+          <label htmlFor="favorite color">Favorite Color</label>
+          <select
+            data-testid="favorite color input"
+            name="favorite color"
+            ref={register({ required: false })}
+          >
+            <option value="blue">blue</option>
+            <option value="red">red</option>
+            <option value="pink">pink</option>
+            <option value="green">green</option>
+            <option value="noneOfTheAbove">none of the above</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="terms">Agree to Terms and Conditions</label>
+          <input
+            data-testid="terms input"
+            name="terms"
+            ref={register({ required: true })}
+            type="checkbox"
+          />
+          {errors.terms && (
+            <p data-testid="terms error">Looks like there was an error: {errors.terms.type}</p>
+          )}
+        </div>
+
         {data && (
           <pre data-testid="return" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
--- src/components/ContractForm.test.js
+++ src/components/ContractForm.test.js
@@ -14,6 +14,8 @@ test("fills out form, submits, checks for return (expected behavior)", async() =
     const email = getByTestId('email input')
     const message = getByTestId('message input')
     const submitButton = getByTestId('submit input')
+    const favoriteColor = getByTestId('favorite color input')
+    const terms = getByTestId('terms input')
 
     // Fill out form inputs
     rtl.fireEvent.change(firstName, {
@@ -32,6 +34,15 @@ test("fills out form, submits, checks for return (expected behavior)", async() =
         target: {name: 'message', value: 'I solemnly swear that I am up to no good'}
     })
 
+    // Additional inputs for stretch
+    rtl.fireEvent.change(favoriteColor, {
+        target: {name: 'favorite color', value: 'green'}
+    })
+
+    rtl.fireEvent.change(terms, {
+        target: {name: 'terms', checked: true}
+    })
+
     // Press submit input
     rtl.fireEvent.click(submitButton)
 
@@ -43,6 +54,8 @@ test("fills out form, submits, checks for return (expected behavior)", async() =
     expect(lastName).toHaveValue('Weasley')
     expect(email).toHaveValue('ronaldweasley@hogwarts.com')
     expect(message).toHaveValue('I solemnly swear that I am up to no good')
+    expect(favoriteColor).toHaveValue('green')
+    expect(terms).toBeChecked()
 })
 
 test("checks for required related errors", async() => {
@@ -51,16 +64,18 @@ test("checks for required related errors", async() => {
     // click submit with empty form to yield errors
     rtl.fireEvent.click(getByTestId('submit input'))
 
-    // assert that errors are rendered
+    // assert that errors are rendered for required inputs
     await findByTestId('firstName error')
     await findByTestId('lastName error')
     await findByTestId('email error')
+    await findByTestId('terms error') // I made terms required
 
-    // This throws an error, so we know we aren't getting data back
+    // This throws an error (when not commented out), so we know we aren't getting data back
     // await findByTestId('return')
    
-
 })
+
+/*  Commented out this test because I've changed the strange behavior of firstName being required to have a a maxmimum of 3 characters and replaced it with a requirement that firstName have at least 3 characters
 
 test("checks for error of firstname being more than three chars long", async() => {
     const { findByTestId, getByTestId } = rtl.render(<ContractForm/>)
@@ -92,7 +107,7 @@ test("checks for error of firstname being more than three chars long", async() =
     rtl.fireEvent.click(submitButton)
 
     // Checks for error
-    const firstNameError = await findByTestId('firstName error')
+    await findByTestId('firstName error')
 
     // This throws breaks the test, so we know we aren't getting data back
     // await findByTestId('return')
@@ -108,4 +123,51 @@ test("checks for error of firstname being more than three chars long", async() =
     // asserted that now data is returned
     await findByTestId('return')
 })
+*/
 
+test("checks for error of firstname being less than three chars long (this is my change)", async() => {
+    const { findByTestId, getByTestId } = rtl.render(<ContractForm/>)
+
+    const firstName = getByTestId('firstName input')
+    const lastName = getByTestId('lastName input')
+    const email = getByTestId('email input')
+    const terms = getByTestId('terms input')
+    const submitButton = getByTestId('submit input')
+
+    // Fill out form inputs
+    rtl.fireEvent.change(firstName, {
+        target: {name: 'firstName', value: 'Ro'}
+    })
+
+    rtl.fireEvent.change(lastName, {
+        target: {name: 'lastName', value: 'Weasley'}
+    })
+
+    rtl.fireEvent.change(email, {
+        target: {name: 'email', value: 'ronaldweasley@hogwarts.com'}
+    })
+
+    rtl.fireEvent.change(terms, {
+        target: {name: 'terms', checked: true}
+    })
+
+    // Press submit input
+    rtl.fireEvent.click(submitButton)
+
+    // Checks for error
+    await findByTestId('firstName error')
+
+    // This throws breaks the test, so we know we aren't getting data back
+    // await findByTestId('return')
+
+    // Change name to abide by 3 chars minimum
+    rtl.fireEvent.change(firstName, {
+        target: {name: 'firstName', value: 'Ron'}
+    })
+
+    // Press submit input
+    rtl.fireEvent.click(submitButton)
+
+    // asserted that now data is returned
+    await findByTestId('return')
+})
